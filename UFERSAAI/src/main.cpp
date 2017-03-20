fe27b858e7a9c94dc2eadff984765194104b2efe
Hugo Dionizio Santos <dnhutos@hotmail.com>
2017-03-20 18:46:00 -0300

    Inicializando projeto UFERSAAI

---
+++ UFERSAAI/src/main.cpp
@@ -0,0 +1,21 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 20/03/2017
+ *      Author: hugo
+ */
+
+#include <iostream>;
+
+using namespace std;
+
+
+int main(int argc, char **argv) {
+
+	cout << "Hello UFERSAAI!" << endl;
+
+	return (0);
+}
+
+
+
