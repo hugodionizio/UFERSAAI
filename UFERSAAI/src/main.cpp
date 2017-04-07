383b345c5703b266c5ba4aef7480d920e48c047e
hugodionizio <dnhutos@hotmail.com>
2017-04-06 21:40:39 -0300

    Inicialização do Backpropagation

--- UFERSAAI/src/main.cpp
+++ UFERSAAI/src/main.cpp
@@ -6,16 +6,36 @@
  */
 
 #include <iostream>;
+#include <cmath>;
 
 using namespace std;
 
-
 int main(int argc, char **argv) {
 
-	cout << "Hello UFERSAAI!" << endl;
+	float A = 0, B = 0, y, expB;
+	float i, j;
+	float wji, ypi, theta_j;
+	bool propagation = true;
+
+
+	cout << "Informe o valor de A: ";
+	cin >> A;
+
+	cout << "\nInforme o valor de B: ";
+	cin >> B;
+
+	wji = 1;
+	ypi = A*B;
+
+	// y(pi) = 1/(1+exp(-sum(iw(ji)*y(pi)+theta(j))))
+	while (propagation) {
+		expB+=  i*wji*ypi+theta_j;
+		propagation = false;
+	}
+
+	y = 1/(1+exp((double)expB));
+
+	cout << y << endl;
 
 	return (0);
 }
-
-
-
