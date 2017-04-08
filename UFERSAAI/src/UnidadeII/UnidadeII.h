478606c6165767f1f110859e0e0d981a3c7c9e18
Hugo Dionizio Santos <hugodionizio@users.noreply.github.com>
2017-04-08 15:40:47 -0300

    Processos de Aprendizagem

--- UFERSAAI/src/UnidadeII/UnidadeII.h
+++ UFERSAAI/src/UnidadeII/UnidadeII.h
@@ -160,6 +160,34 @@ void setG(Perceptron *, float, float); // saída desejada - saída obtida
 void setCorrecaoEntrada(Perceptron *, int);
 void setNovoPeso(Perceptron *, int);
 
+//	Processos de Aprendizagem
+//		Adaptação dos parâmetros livres através do ambiente
+void estimularRede(RedeNeural *);
+void respostaEstimuloRede(RedeNeural *);
+void atualizarRedeNeural(RedeNeural *);
+
+//		Regras de Aprendizagem
+float custo(RedeNeural);
+float minimizar(RedeNeural);
+void correcaoErro(RedeNeural *);
+
+void hebbiano(RedeNeural *); // fortalecimento ou enfraquecimento de sinapase entre neurônios
+
+void competitivo(RedeNeural *);
+
+void boltzmann(RedeNeural *);
+
+//		Paradigmas de Aprendizagem
+enum Paradigma {
+	SUPERVISIONADO,
+	NAOSUPERVISIONADO,
+	REFORCO
+};
+
+void reforcoPrimario(RedeNeural *);
+void reforcoHeuristico(RedeNeural *);
+void reforco(RedeNeural *);
+
 //		Multi Layer Perceptron (MLP)
 float backpropagation(float, float);
 
